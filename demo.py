55743090b45c92d1e5f28d0256a9fa7e141878a2
Samar <137534422+Samarraj11@users.noreply.github.com>
2023-08-11 00:02:59 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,12 @@
+import string
+import random
+
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter the length of the password: "))
+    password = generate_random_password(password_length)
+    print("Randomly generated password:", password)
